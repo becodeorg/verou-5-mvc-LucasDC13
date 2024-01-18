421b7a1e07a54271788aa2f7202a210a5c137cad
LucasDC13 <lucas_de_corte@hotmail.com>
2024-01-18 14:42:29 +0100

    Add gitignore

--- Controller/ArticleController.php
+++ Controller/ArticleController.php
@@ -1,6 +1,43 @@
 <?php
 
 declare(strict_types = 1);
+require_once "config.php";
+
+class DatabaseManager
+{
+    private string $host;
+    private string $user;
+    private string $password;
+    private string $dbname;
+
+    public PDO $connection;
+
+    public function __construct(string $host, string $user, string $password, string $dbname)
+    {
+        $this->host = $host;
+        $this->user = $user;
+        $this->password = $password;
+        $this->dbname = $dbname;
+        // TODO: Set any user and password information
+    }
+
+    public function connect(): void
+    {
+        try {
+            $dsn = "mysql:host=$this->host;dbname=$this->dbname";
+            $this->connection = new PDO($dsn, $this->user, $this->password);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        } catch (PDOException $error) {
+            echo $error->getMessage();
+        }
+
+    }
+}
+
+$databaseManager = new DatabaseManager($config['host'], $config['user'], $config['password'], $config['dbname']);
+$databaseManager->connect();
+
 
 class ArticleController
 {
